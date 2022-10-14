144c3369bacf6ff21d1d105dd3a9e8c6fb724e87
MathCoder <mathieufc31@gmail.com>
2022-10-14 18:47:44 +0200

    features creating user, logging done

--- app.js
+++ app.js
@@ -14,13 +14,7 @@ const users = require("./routes/users");
 app.use("/", posts);
 app.use("/", users);
 
-mongoose.connect(
-  process.env.DB_CONNECTION,
-  { useNewUrlParser: true },
-  () =>
-    console.log(
-      `Connection ${new Date().getHours()}:${new Date().getMinutes()}`
-    )
-  
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log(`Connection ${new Date().getHours()}:${new Date().getMinutes()}`)
 );
 app.listen(3000);
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 router.get("/login", async (req, res) => {
   try {
@@ -11,20 +12,49 @@ router.get("/login", async (req, res) => {
   }
 });
 
-router.post("/register", (req, res) => {
-  const post = new User({
-    username: req.body.username,
-    password: req.body.password,
-  });
-
-  post
-    .save()
-    .then(() => {
-      res.json({ message: "success" });
-    })
-    .catch((error) => {
-      res.json({ message: error });
+router.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = new User({
+      username: req.body.username,
+      // password: req.body.password,
+      password: hashedPassword,
     });
+
+    user
+      .save()
+      .then(() => {
+        res.json({ message: "success" });
+      })
+      .catch((error) => {
+        res.json({ message: error });
+      });
+  } catch (error) {}
+});
+
+router.post("/login", (req, res) => {
+  User.findOne({ username: req.body.username }, async (err, user) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      return;
+    }
+    if (!user) {
+      res.json({
+        status: 401,
+        msg: "not found",
+      });
+    } else {
+      try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+          res.send("success");
+        } else {
+        }
+      } catch {
+        res.status(401).send("not found");
+      }
+    }
+  });
 });
 
 // router.delete("/:postId", async (req, res) => {
