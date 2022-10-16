5b8c6e03a08fb29f5777152db3592cdfce29d7f1
MathCoder <mathieufc31@gmail.com>
2022-10-16 22:51:04 +0200

    update authenticate token middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 
 function authenticateToken(req, res, next) {
   const token = req.headers["authorization"].split(' ')[1];
+  console.log("token =>",token);
   if (token == null) {
     res.status(401);
     res.send("No token");
