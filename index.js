e1a52d2d63e74e7ed1e7b9c1f66d4c361a62485c
MathCoder <mathieufc31@gmail.com>
2022-10-17 10:03:39 +0200

    try vercel

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ require("dotenv/config");
 app.use(cors());
 app.use(bodyParser.json());
 
-const posts = require("./routes/posts");
-const users = require("./routes/users");
+const posts = require("./api/posts");
+const users = require("./api/users");
 
-app.use("/posts", posts);
-app.use("/auth", users);
+app.use("/api/posts", posts);
+app.use("/api/auth", users);
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
   console.log(`Connection ${new Date().getHours()}:${new Date().getMinutes()}`)
