1183185d383ed15b44636f09dcf025191b03c7ac
MathCoder <mathieufc31@gmail.com>
2022-10-17 20:50:13 +0200

    move db connection

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv/config");
 
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log(`Connection ${new Date().getHours()}:${new Date().getMinutes()}`)
+);
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -14,7 +18,5 @@ const users = require("./routes/users");
 app.use("/posts", posts);
 app.use("/auth", users);
 
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
-  console.log(`Connection ${new Date().getHours()}:${new Date().getMinutes()}`)
-);
+
 app.listen(process.env.PORT || 3000);
