107a5b2232a697c937f512b8394f263a378d937c
MathCoder <mathieufc31@gmail.com>
2022-10-28 04:40:43 +0200

    patch the bug in login that was making impossible to call login twice

--- routes/users.js
+++ routes/users.js
@@ -59,77 +59,75 @@ router.post("/register", (req, res) => {
 });
 
 // OK
-router.post("/login", async (req, res) => {
-  User.findOne({ username: req.body.username }, async (err, user) => {
-    if (err) {
-      res.status(401);
-      res.send(`Not acceptable => ${err}`);
-      return;
-    }
-    if (!user) {
-      res.status(403);
-      res.send("User not found");
-    } else {
-      try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-          await bcrypt.compare(req.body.password, user.password);
-          console.log("compare", user.password);
-          const userData = {
-            username: req.body.username,
-            password: user.password,
-          };
+router.post("/login", (req, res) => {
+  console.log("test 1");
+  User.findOne({ username: req.body.username })
+    .then(async (user) => {
+      if (!user) {
+        res.status(403);
+        res.send("User not found");
+      } else {
+        try {
+          if (await bcrypt.compare(req.body.password, user.password)) {
+            await bcrypt.compare(req.body.password, user.password);
+            console.log("compare", user.password);
+            const userData = {
+              username: req.body.username,
+              password: user.password,
+            };
 
-          if (user.refreshToken == "") {
-            const newToken = generateRefreshToken(userData);
-            User.updateOne(
-              { username: req.body.username },
-              { refreshToken: newToken },
-              function (err) {
-                if (err) {
-                  res.status(403);
-                  res.send(`Fail to update => ${err}`);
+            if (user.refreshToken == "") {
+              const newToken = generateRefreshToken(userData);
+              User.updateOne(
+                { username: req.body.username },
+                { refreshToken: newToken },
+                function (err) {
+                  if (err) {
+                    res.status(403);
+                    res.send(`Fail to update => ${err}`);
+                  }
                 }
-              }
-            );
-          }
+              );
+            }
 
-          if (!user.refreshToken) {
-            const newToken = generateRefreshToken(userData);
-            User.updateOne(
-              { username: req.body.username },
-              { refreshToken: newToken },
-              function (err) {
-                if (err) {
-                  res.status(403);
-                  res.send(`Fail to update => ${err}`);
+            if (!user.refreshToken) {
+              const newToken = generateRefreshToken(userData);
+              User.updateOne(
+                { username: req.body.username },
+                { refreshToken: newToken },
+                function (err) {
+                  if (err) {
+                    res.status(403);
+                    res.send(`Fail to update => ${err}`);
+                  }
                 }
-              }
-            );
-          }
-
-          // There's a weird bug, the jwt func is called before the two func above
-          // and then it says it's impossible to get a token but a new one is created
-          try {
-            jwt.verify(user.refreshToken, process.env.REFRESH_TOKEN_SECRET);
-            const accessToken = generateAccessToken(userData);
-            res.status(200);
-            res.send({ accessToken: accessToken });
-          } catch (err) {
-            res.status(200);
-            res.send("Success to verify");
+              );
+            }
+            try {
+              jwt.verify(user.refreshToken, process.env.REFRESH_TOKEN_SECRET);
+              const accessToken = generateAccessToken(userData);
+              res.status(200);
+              res.send({ accessToken: accessToken });
+            } catch (err) {
+              res.status(200);
+              res.send("Success to verify");
+            }
+            // works
+          } else {
+            res.status(403);
+            res.send("Wrong credentials");
           }
           // works
-        } else {
+        } catch {
           res.status(403);
-          res.send("Wrong credentials");
+          res.send(`One of the required credientials is missing => ${err}`);
         }
-        // works
-      } catch {
-        res.status(403);
-        res.send(`One of the required credientials is missing => ${err}`);
       }
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(401);
+      res.send(`Not acceptable => ${err}`);
+    });
 });
 
 // OK
