32f69986a79cb809415a5c2892fbff5ae7db7204
MathCoder <mathieufc31@gmail.com>
2022-10-28 04:43:48 +0200

    put back split in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 function authenticateToken(req, res, next) {
-  const token = req.headers["authorization"];
+  const token = req.headers["authorization"].split(' ')[1];
   if (token == null) {
     res.status(401);
     res.send("No token");
