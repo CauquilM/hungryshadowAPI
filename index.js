7c4214daa321a36723041e88251433aade77abcf
MathCoder <mathieufc31@gmail.com>
2023-01-19 13:06:07 +0100

    rename app.js

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log(`Connection ${new Date().getHours()}:${new Date().getMinutes()}`)
+);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const posts = require("./routes/posts");
+const users = require("./routes/users");
+
+app.use("/posts", posts);
+app.use("/auth", users);
+
+app.listen(process.env.PORT || 3000);
